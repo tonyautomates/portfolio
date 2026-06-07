4a3876b21a538b73b7298697f9323804158a71c1
Antoni <antoni.gaj2010@gmail.com>
2026-06-07 17:29:06 +0200

    Add dark mode selector

--- script.js
+++ script.js
@@ -1,8 +1,41 @@
 const menuButton = document.getElementById("menuButton");
 const navLinks = document.getElementById("navLinks");
+const themeButton = document.getElementById("themeButton");
+
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "dark") {
+  document.body.classList.add("dark-mode");
+}
+
+updateThemeButtonText();
 
 if (menuButton && navLinks) {
   menuButton.addEventListener("click", function () {
     navLinks.classList.toggle("open");
   });
+}
+
+if (themeButton) {
+  themeButton.addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+      localStorage.setItem("theme", "dark");
+    } else {
+      localStorage.setItem("theme", "light");
+    }
+
+    updateThemeButtonText();
+  });
+}
+
+function updateThemeButtonText() {
+  if (!themeButton) return;
+
+  if (document.body.classList.contains("dark-mode")) {
+    themeButton.textContent = "Light";
+  } else {
+    themeButton.textContent = "Dark";
+  }
 }
